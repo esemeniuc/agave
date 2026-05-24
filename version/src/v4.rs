ca9b49dbbbad00f9e507a1b2c50e50b3cb52c0e5
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-05-24 08:30:21 -0700

    add client id switch

--- version/src/v4.rs
+++ version/src/v4.rs
@@ -223,6 +223,10 @@ impl Version {
         &self.client
     }
 
+    pub fn set_client(&mut self, client: ClientId) {
+        self.client = client;
+    }
+
     pub fn prerelease(&self) -> &Prerelease {
         &self.prerelease
     }
