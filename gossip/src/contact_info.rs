2b70bccb4a2a7418260a7b4edfd2d4696b64cabb
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-05-24 08:30:21 -0700

    add client id switch

--- gossip/src/contact_info.rs
+++ gossip/src/contact_info.rs
@@ -243,6 +243,10 @@ impl ContactInfo {
         &self.version
     }
 
+    pub(crate) fn set_version_client(&mut self, client: solana_version::ClientId) {
+        self.version.set_client(client);
+    }
+
     pub fn hot_swap_pubkey(&mut self, pubkey: Pubkey) {
         self.pubkey = pubkey;
         // Need to update ContactInfo.outset so that this node's contact-info
